a3c2e94f66e23b52ec483abb11e4c9a9743fbfa9
Beinrain06 <elngoueni@gmail.com>
2024-07-09 15:52:19 -0400

    refactor about-me mini mobile page v1

--- src/components/about.ts
+++ src/components/about.ts
@@ -29,7 +29,7 @@ export default class About implements IAbout {
 
     row.className = "locker_row row w-100 h-100 mx-auto";
 
-    dutyWrapper.className = "duty_wrapper w-100 h-100";
+    dutyWrapper.className = "duty_wrapper";
 
     const skillsRecap = document.createElement("div") as HTMLDivElement;
 
@@ -38,7 +38,7 @@ export default class About implements IAbout {
     skillsRecap.innerHTML = `
     <div id="first_skill_pro" class="skill_item_pro">
           <label for="css">CSS</label>
-          <div id="progress_css" class="progress style="height: 16px" role="progressbar" aria-label="Example with label" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
+          <div id="progress_css" class="progress style="height: 14px" role="progressbar" aria-label="Example with label" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
           <div class="progress-bar" style="width: 40%;background-color: #f4f4f4; border-radius: 30px;animation: css-progress-anim 1s ease-in-out 4s forwards">75%</div>
         </div>
     
@@ -49,17 +49,17 @@ export default class About implements IAbout {
         </div>
         <div class="skill_item_pro">
           <label for="express">ExpressJS</label>
-          <div id="progress_express" class="progress style="height: 16px" role="progressbar" aria-label="express" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100">
+          <div id="progress_express" class="progress style="height: 14px" role="progressbar" aria-label="express" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100">
           <div class="progress-bar" style="width: 55%;background-color: #f4f4f4; border-radius: 30px;animation: express-progress-anim 1s ease-in-out 6s forwards;">55%</div>
         </div>
         <div class="skill_item_pro">
           <label for="typescript">Typescript</label>
-          <div id="progress_typescript" class="progress style="height: 16px" role="progressbar" aria-label="typescript" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+          <div id="progress_typescript" class="progress style="height: 14px" role="progressbar" aria-label="typescript" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
           <div class="progress-bar" style="width: 25%; background-color: #f4f4f4;border-radius: 30px;animation: typescript-progress-anim 1s ease-in-out 7s forwards;">25%</div>
         </div>
         <div class="skill_item_pro">
           <label for="node">NodeJS</label>
-          <div id="progress_node" class="progress style="height: 16px" role="progressbar" aria-label="NodeJs" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">
+          <div id="progress_node" class="progress style="height: 14px" role="progressbar" aria-label="NodeJs" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">
           <div class="progress-bar" style="width: 20%;background-color: #f4f4f4; border-radius: 30px;animation: node-progress-anim 1s ease-in-out 8s forwards;">20%</div>
         </div>
     `;
@@ -70,23 +70,10 @@ export default class About implements IAbout {
     <div class="agent_img"></div>
     `;
 
-    dutyWrapper.appendChild(agentImgWrapper);
-    dutyWrapper.appendChild(skillsRecap);
-
-    row.appendChild(dutyWrapper);
-    container.appendChild(row);
-
-    this.MainAbout.innerHTML = "";
-    this.MainAbout.appendChild(container);
-  }
-
-  detailsInterest() {
-    const container = document.createElement("div") as HTMLDivElement;
-    const row = document.createElement("div") as HTMLDivElement;
-
-    container.className = "interest_container container-fluid";
-
-    row.className = "interest_row row";
+    const skillsAndAgentWrapper = document.createElement(
+      "div"
+    ) as HTMLDivElement;
+    skillsAndAgentWrapper.className = "skills_agent_wrap";
 
     const workingArea = document.createElement("div") as HTMLDivElement;
 
@@ -96,6 +83,28 @@ export default class About implements IAbout {
     <p class="skills_acquaintance fw-bold" >Front-End/ Back-End Developer </p>
     `;
 
+    skillsAndAgentWrapper.appendChild(agentImgWrapper);
+    skillsAndAgentWrapper.appendChild(skillsRecap);
+
+    dutyWrapper.appendChild(workingArea);
+    dutyWrapper.appendChild(skillsAndAgentWrapper);
+
+    row.appendChild(dutyWrapper);
+
+    container.appendChild(row);
+
+    this.MainAbout.innerHTML = "";
+    this.MainAbout.appendChild(container);
+  }
+
+  detailsInterest() {
+    const container = document.createElement("div") as HTMLDivElement;
+    /* const row = document.createElement("div") as HTMLDivElement; */
+
+    container.className = "interest_container container-fluid";
+
+    /*  row.className = "interest_row row"; */
+
     const interestBoard = document.createElement("div") as HTMLDivElement;
 
     interestBoard.className = "inner_side_interests";
@@ -103,14 +112,14 @@ export default class About implements IAbout {
     interestBoard.innerHTML = `
       <div class="beating_interest">
         <span class="int_work_title">Interest</span>
-        <p class="interest_area" > animes, writing, movies, Games  </p>
-      </div> "
+        <p class="interest_area" >  writing, movies, Games  </p>
+      </div> 
       `;
 
-    row.appendChild(workingArea);
-    row.appendChild(interestBoard);
+    /* row.appendChild(workingArea); */
+    /*  row.appendChild(interestBoard); */
 
-    container.appendChild(row);
+    container.appendChild(interestBoard);
 
     this.MainAbout.appendChild(container);
   }
