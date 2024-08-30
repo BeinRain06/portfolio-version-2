fdcaf5952e69d5090f37b664d7e85b961832edf5
Beinrain06 <elngoueni@gmail.com>
2024-08-30 13:26:43 -0400

    delete all files related vanilla-js

--- src/assets/images/index.ts
+++
@@ -1,5 +0,0 @@
-import linkedin from "./linkedin-svgrepo-com.svg";
-import github from "./logo-github-svgrepo-com.svg";
-import twitter from "./twitter-svgrepo-com.svg";
-
-export { linkedin, github, twitter };
